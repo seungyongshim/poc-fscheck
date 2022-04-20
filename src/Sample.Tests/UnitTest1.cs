5f75bdbf2eccd4163d8eda6a3e43c1aef427a508
SeungYongShim <syshim@me.com>
2022-04-20 23:28:06 +0900

    코드 수정

--- src/Sample.Tests/UnitTest1.cs
+++ src/Sample.Tests/UnitTest1.cs
@@ -1,8 +1,6 @@
-using System;
 using FsCheck;
 using FsCheck.Fluent;
 using FsCheck.Xunit;
-using Xunit;
 using static Sample.Prelude;
 
 namespace Sample.Tests;
@@ -14,6 +12,7 @@ public class PreludeSpec
         (x * 2 == add(x)(x)).ToProperty();
 
     [Property]
-    public Property Adding_Tow_Number_Doesnt_Order(int x, int y) =>
-        (add(x)(y) == add(y)(x)).ToProperty();
+    public Property Adding_Tow_Number_Doesnt_Order() =>
+        Prop.ForAll((int x, int y) => add(x)(y) == add(y)(x));
+            
 }
